e54635f514caf89c2a3f758324185fc995742531
Darina Rustamova <darisha6602@gmail.com>
2024-09-08 19:27:29 +0300

    feat: update poetry.lock & add list iteration for journal

--- parsing/parse_journal.py
+++ parsing/parse_journal.py
@@ -2,31 +2,28 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from pdf.scripts.book_1 import process_string, should_skip_string
 
 
-def parse_html_to_csv(urls, csv_file):
+def parse_html_to_csv(urls: list[str], csv_file: str):
     data = {"source": [], "target": []}
 
     for url in urls:
         try:
             response = requests.get(url)
-            response.raise_for_status()  # Проверка, что запрос успешен
+            response.raise_for_status()
 
-            # Парсим HTML с помощью BeautifulSoup
             soup = BeautifulSoup(response.content, "html.parser")
 
-            # Извлекаем необходимую информацию
             manskiy_div = soup.find("div", class_="box visible")
             russkiy_div = soup.find(
                 "div",
                 class_="field field-name-field-body-russian field-type-text-with-summary field-label-hidden",
             )
-            print(manskiy_div)
             if not manskiy_div or not russkiy_div:
                 print(f"Не удалось найти нужные элементы на странице {url}")
                 continue
 
-            # Получаем тексты
             source_text = " ".join(
                 [p.get_text(strip=True) for p in manskiy_div.find_all("p")]
             )
@@ -34,26 +31,28 @@ def parse_html_to_csv(urls, csv_file):
                 [p.get_text(strip=True) for p in russkiy_div.find_all("p")]
             )
 
-            data["source"].append(source_text)
-            data["target"].append(target_text)
+            if not should_skip_string(source_text) and not should_skip_string(
+                target_text
+            ):
+                data["source"].append(process_string(source_text))
+                data["target"].append(process_string(target_text))
 
             print(f"Успешно обработан URL: {url}")
         except Exception as e:
             print(f"Ошибка при обработке URL {url}: {e}")
 
-    # Формируем DataFrame
     df = pd.DataFrame(data)
 
-    # Сохраняем в CSV
     df.to_csv(csv_file, mode="a", index=False, header=False, encoding="utf-8")
 
 
-def generate_urls(no_first_max: int, no_second_min: int):
+def generate_urls(no_journal: list[int], count: int = 24):
     base_url = "https://www.khanty-yasang.ru/luima-seripos/no-{}-{}"
     urls = []
 
-    for issue in range(no_second_min, no_second_min + no_first_max):
-        urls.append(base_url.format(issue - no_second_min + 1, issue))
+    for no_j in no_journal:
+        for issue in range(no_j, no_j + count):
+            urls.append(base_url.format(issue - no_j + 1, issue))
 
     return urls
 
@@ -63,12 +62,24 @@ def main():
         description="Parse HTML from multiple URLs and save specific data to a CSV file."
     )
     parser.add_argument("--csv_file", type=str, help="Output CSV file path")
-    parser.add_argument("--no-first-max", type=int)
-    parser.add_argument("--no-second-min", type=int)
     args = parser.parse_args()
+    no_journal = [
+        1011,
+        1043,
+        1067,
+        1091,
+        1115,
+        1139,
+        1163,
+        1187,
+        1211,
+        1235,
+        1259,
+        1283,
+        1307,
+    ]
 
-    # Генерируем URL и вызываем функцию для парсинга и сохранения данных
-    urls = generate_urls(args.no_first_max, args.no_second_min)
+    urls = generate_urls(no_journal)
     parse_html_to_csv(urls, args.csv_file)
 
     print(f"Данные успешно сохранены в {args.csv_file}")
